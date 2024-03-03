cd372a206a6e13a7ab1ecac871913ee8b6fcaee5
d-alejandro-salas <daniel_cabj88@hotmail.com>
2024-03-03 18:40:37 -0300

    Mejorado el main de AboutMe y la galería

--- JS/halloweenButton.js
+++ JS/halloweenButton.js
@@ -1,26 +1,28 @@
 "use strict";
 
-// Agregar evento de clic al botón para cambiar entre los modos Halloween y Normal
+const mainText = document.getElementById('aboutMe__main');
 const switchButton = document.createElement("button");
-switchButton.className = "light-mode-button";
-switchButton.id = "switch";
-switchButton.innerHTML = '<span class="icon moon" style="color:#FFF; font-family: Creepster; letter-spacing: 2px;">HALLOWEEN MODE</span>';
+switchButton.id = "halloweenSwitch";
+switchButton.innerHTML = 'HALLOWEEN MODE';
+let isHalloweenMode = false;
 
 switchButton.addEventListener("click", function () {
-  document.body.classList.toggle('light'); // Alternar la clase 'light' en el body
-
+  // Alternar la clase 'halloween' para aplicar estilos
+  switchButton.classList.toggle('halloween');
+  // Alternar entre los modos Halloween y Normal
+  isHalloweenMode = !isHalloweenMode;
   // Cambiar el contenido del botón
-  const modeText = switchButton.querySelector('.icon.moon'); // Cambiar a .icon.moon
-  if (modeText.textContent === 'HALLOWEEN MODE') { // Cambiar a 'HALLOWEEN MODE'
-    modeText.textContent = 'NORMAL MODE';
-    modeText.style.fontFamily = "Arial, sans-serif";
-    modeText.style.color = "black";
+  if (isHalloweenMode) {
+    switchButton.innerHTML = 'NORMAL MODE';
+    mainText.style.fontFamily = 'Creepster';
+    mainText.style.color = 'red'
   } else {
-    modeText.textContent = 'HALLOWEEN MODE';
-    modeText.style.fontFamily = "Creepster, sans-serif";
-    modeText.style.color = "white";
+    switchButton.innerHTML = 'HALLOWEEN MODE';
+    mainText.style.fontFamily = 'inherit'
+    mainText.style.color = 'inherit'
   }
 });
+
 
 const main = document.querySelector("main");
 main.appendChild(switchButton);
